e7f4311a3cafadd1d2f37e662a01e9bc022140d4
Kana Ali <kanagataliomrov@gmail.com>
2025-12-11 03:56:01 +0000

    logoya şu resmi koy

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function EcbLogo() {
   return (
     <Image
-      src="https://storage.googleapis.com/aai-web-samples/logo-white-1.png"
+      src="https://storage.googleapis.com/aai-web-samples/logo.png"
       alt="ECB Global Logo"
       width={160}
       height={40}
