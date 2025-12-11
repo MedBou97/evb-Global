cf9a5a91f558ba007df932efe910065f7d9cfbc7
Kana Ali <kanagataliomrov@gmail.com>
2025-12-11 02:54:44 +0000

    Fontu Mulish'e dönüştür ve yazıları biraz büyüt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
         headline: ['"Playfair Display"', 'serif'],
         logo: ['"Cinzel"', 'serif'],
         code: ['monospace'],
-        submenu: ['"Jost"', 'sans-serif'],
+        submenu: ['"Mulish"', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
